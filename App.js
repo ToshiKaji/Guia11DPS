e81554b11a6c374375a45ae780e6651b9af49a7a
ToshiKaji <toshicaballero@outlook.com>
2023-10-10 08:41:56 -0600

    subida de guia

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React,{useState,useEffect} from 'react';
+import { StyleSheet, Text, View,Alert } from 'react-native';
+import Formulario from './src/components/Formulario';
+import Pais from './src/components/pais';
 export default function App() {
+
+const [busqueda,guardarbusqueda]=useState({pais:''})
+
+const [consultar, guardarconsultar] = useState(false);
+const [resultado, guardarresultado] = useState({});
+useEffect(() => {
+const { pais } = busqueda;
+const consultarPais = async () => {
+if (consultar) {
+const url =
+`https://servicodados.ibge.gov.br/api/v1/paises/${pais}`;
+try {
+const respuesta = await fetch(url);
+const resultado = await respuesta.json();
+guardarresultado(resultado);
+guardarconsultar(false);
+} catch (error) {
+mostrarAlerta();
+}
+}
+};
+consultarPais();
+},[consultar]);
+const mostrarAlerta = () => {
+Alert.alert('Error', 'No hay resultado intenta con otra ciudad o país'),[{ Text: 'Ok' }];
+};
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.app}>
+    <View style={styles.contenido}>
+    <Formulario
+    busqueda={busqueda}
+    guardarbusqueda={guardarbusqueda}
+    guardarconsultar={guardarconsultar}
+    />
+    <Pais resultado={resultado} />
+    </View>
     </View>
   );
 }
@@ -18,4 +52,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  app: {
+    flex: 1,
+    backgroundColor: 'rgb(71,149,212)',
+    justifyContent: 'center',
+    },
+    contenido: {
+    margin: '2.5%',
+    },
+    
 });
